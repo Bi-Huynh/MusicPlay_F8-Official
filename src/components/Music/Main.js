36a2f2456250d7d39cfd38886ec0a1dd82777a01
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2021-03-06 22:02:39 +0700

    install package node-scss, fix link css and link mp3 img in data song, use express.static

--- src/components/Music/Main.js
+++ src/components/Music/Main.js
@@ -7,17 +7,25 @@ const app = express();
 
 
 const port = 3000;
+// đường dẫn tuyệt đối đến file src
+const pathFileSrc = __dirname.slice(0, __dirname.length - 17);
+// __dirname : đường dẫn tuyệt đối tới file hiện hành - js cung cấp
+// đường dẫn tuyệt đối đến file AppMusicPlay
+const pathApp = __dirname.slice(0, __dirname.length - 20);
+
+
 
 // Sử dụng morgan - http loger
 app.use(morgan('combined'));
+// Sử dụng express.static để có thể sử dụng được các file img, mp3, ....
+app.use(express.static(path.join(pathApp, 'public')));
 
 // sử dụng handlebars - template engine - có nhiều template engine khác nữa: pug, fs,.... 
 app.engine('hbs', handlebars({
     extname: '.hbs' // cấu hình đuôi file, cho đuôi file khỏi dài
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname.slice(0, __dirname.length - 17), 'resources/views'));
-
+app.set('views', path.join(pathFileSrc, 'resources/views'));
 
 app.get('/', (req, res) => res.render('home'));
 
